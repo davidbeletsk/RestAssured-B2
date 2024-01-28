8b4a35d90bfa42847f94035ae52a61ac1045d1e3
davydbeletsk <david37898@gmail.com>
2024-01-28 13:15:08 -0500

    apiiii

---
+++ src/test/java/io/loopcamp/test/day04_a_json_path/HREmployeesJsonPathTest.java
@@ -0,0 +1,14 @@
+package io.loopcamp.test.day04_a_json_path;
+
+public class HREmployeesJsonPathTest {
+
+    /**
+     Given accept type is Json
+     And query param limit is 200
+     when I send GET request to /employees
+     Then I can use jsonPath to query and read values from json body
+     */
+
+
+
+}
